b04ff2f76e50f5df6cfdc99c3992a6f4f02627fb
Eric Swotinsky and Nanette Girzi <nanette@>
2018-03-14 16:26:14 -0700

    add more styling, add reset button

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(function() {
+
   $('#checker-form').submit(function(event){
     event.preventDefault();
     let lineOne = $("#lineOne").val();
@@ -12,22 +13,14 @@ $(function() {
     let lineThree = $("#lineThree").val();
     let newHaiku = new haikuChecker(lineOne, lineTwo, lineThree);
 
-    if (newHaiku.syllableCounter(lineOne) == 5){
-      console.log("line one success");
-    }
-    if (newHaiku.syllableCounter(lineTwo) == 7){
-      console.log("line two success");
-    }
-    if (newHaiku.syllableCounter(lineThree) == 5){
-      console.log("line three success");
-    }
-
     if((newHaiku.syllableCounter(lineOne) == 5) && (newHaiku.syllableCounter(lineTwo) == 7) && (newHaiku.syllableCounter(lineThree) == 5)){
       $('#results').text("Congrats, you wrote a haiku!");
     }
     else {
-      $('#results').text("Whoops! A haiku is a poem with three lines, where the first line is 5 syllables, the second line is 7 syllables, and the third line is 5 syllables. Try again.");
+      $('#results').html(`<p>Whoops! A haiku is a poem with three lines, where the first line is 5 syllables, the second line is 7 syllables, and the third line is 5 syllables.</p><p>Your haiku had ${newHaiku.syllableCounter(lineOne)} syllables in the first line, ${newHaiku.syllableCounter(lineTwo)} syllables in the second line, and ${newHaiku.syllableCounter(lineThree)} syllables in the third line.</p><p>Try again.</p>`);
     }
-
   });
+    $("#resetButton").click(function(){
+      $("#results").text("");
+    });
 });
